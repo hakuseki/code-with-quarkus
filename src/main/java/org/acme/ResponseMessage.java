6628596315391e7dee69bdbd52f4bc705fccf98e
Mikael Andersson Wigander <mikael.andersson.wigander@pm.me>
2024-03-21 07:49:44 +0100

    First commit

---
+++ src/main/java/org/acme/ResponseMessage.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+
+@Data
+@JsonSerialize
+public class ResponseMessage {
+
+    private int status = 0;
+
+    private String message = "Success";
+
+    public void setError(final int statusCode, final String responseMessage) {
+        status = statusCode;
+        message = responseMessage;
+    }
+}
